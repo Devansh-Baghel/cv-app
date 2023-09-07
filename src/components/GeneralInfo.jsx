ea7572d732f4827539f105390b4ea863ee63385c
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-07 10:45:10 +0530

    add preview in GeneralInfo.jsx

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,47 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function GeneralInfo() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [cvData, setCvData] = useState(null);
   const [isEditing, setIsEditing] = useState(true);
 
   const handleSubmit = () => {
-    setIsEditing(false);
+    const cvInfo = {
+      name,
+      email,
+      phone,
+    };
+
+    setCvData(cvInfo);
+    setIsEditing(false); // Exit edit mode after submitting
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true); // Enter edit mode
   };
 
   return (
     <div>
-      {isEditing ? (
-        <div>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <button onClick={handleSubmit}>Submit</button>
-        </div>
-      ) : (
-        <div>
-          <p>Name: {name}</p>
-          <p>Email: {email}</p>
-          <p>Phone: {phone}</p>
-          <button onClick={() => setIsEditing(true)}>Edit</button>
-        </div>
-      )}
+      <div>
+        <h2>General Information</h2>
+        {isEditing ? (
+          <div>
+            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+            {/* <button onClick={handleSubmit}>Submit</button> */}
+          </div>
+        ) : (
+          <div>
+            <p>Name: {cvData ? cvData.name : ''}</p>
+            <p>Email: {cvData ? cvData.email : ''}</p>
+            <p>Phone: {cvData ? cvData.phone : ''}</p>
+          </div>
+        )}
+        <button onClick={isEditing ? handleSubmit : handleEdit}>{isEditing ? 'Submit' : 'Edit'}</button>
+      </div>
+      <div>
+        <h2>CV Preview</h2>
+        {cvData ? (
+          <div>
+            <p>Name: {cvData.name}</p>
+            <p>Email: {cvData.email}</p>
+            <p>Phone: {cvData.phone}</p>
+          </div>
+        ) : (
+          <p>Please submit to see your CV preview.</p>
+        )}
+      </div>
     </div>
   );
 }
