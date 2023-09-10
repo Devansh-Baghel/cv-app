a12c70468562a11e89b6a658806304b3188553d8
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-11 00:30:17 +0530

    Added Seperated preview into its own component

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Preview from "./Preview";
 
 function Education() {
-  const [title, setTitle] = useState('');
-  const [schoolName, setSchoolName] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [title, setTitle] = useState("");
+  const [schoolName, setSchoolName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [cvData, setCvData] = useState(null);
   const [isEditing, setIsEditing] = useState(true);
 
@@ -13,7 +14,7 @@ function Education() {
       title,
       schoolName,
       startDate,
-      endDate
+      endDate,
     };
 
     setCvData(cvInfo);
@@ -30,35 +31,45 @@ function Education() {
         <h2>Education</h2>
         {isEditing ? (
           <div>
-            <input type="text" placeholder="Title of Study" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <input type="text" placeholder="Name of Acedemic Institution" value={schoolName} onChange={(e) => setSchoolName(e.target.value)} />
-            <input type="text" placeholder="Start Date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-            <input type="text" placeholder="End Date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Title of Study"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Name of Acedemic Institution"
+              value={schoolName}
+              onChange={(e) => setSchoolName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Start Date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="End Date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
             {/* <button onClick={handleSubmit}>Submit</button> */}
           </div>
         ) : (
           <div>
-            <p>Title: {cvData ? cvData.title : ''}</p>
-            <p>School Name: {cvData ? cvData.schoolName : ''}</p>
-            <p>Start Date: {cvData ? cvData.startDate : ''}</p>
-            <p>End Date: {cvData ? cvData.endDate : ''}</p>
+            <p>Title: {cvData ? cvData.title : ""}</p>
+            <p>School Name: {cvData ? cvData.schoolName : ""}</p>
+            <p>Start Date: {cvData ? cvData.startDate : ""}</p>
+            <p>End Date: {cvData ? cvData.endDate : ""}</p>
           </div>
         )}
-        <button onClick={isEditing ? handleSubmit : handleEdit}>{isEditing ? 'Submit' : 'Edit'}</button>
+        <button onClick={isEditing ? handleSubmit : handleEdit}>
+          {isEditing ? "Submit" : "Edit"}
+        </button>
       </div>
-      <div>
-        <h2>CV Preview</h2>
-        {cvData ? (
-          <div>
-            <p>Title: {cvData.title}</p>
-            <p>School Name: {cvData.schoolName}</p>
-            <p>Start Date: {cvData.startDate}</p>
-            <p>End Date: {cvData.endDate}</p>
-          </div>
-        ) : (
-          <p>Please submit to see your CV preview.</p>
-        )}
-      </div>
+      <Preview educationData={cvData} />
     </div>
   );
 }
--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Preview from "./Preview";
 
 function GeneralInfo() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [cvData, setCvData] = useState(null);
   const [isEditing, setIsEditing] = useState(true);
 
@@ -22,37 +23,43 @@ function GeneralInfo() {
     setIsEditing(true); // Enter edit mode
   };
 
+  <Preview generalData={cvData} />;
   return (
     <div>
       <div>
         <h2>General Information</h2>
         {isEditing ? (
           <div>
-            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
             {/* <button onClick={handleSubmit}>Submit</button> */}
           </div>
         ) : (
           <div>
-            <p>Name: {cvData ? cvData.name : ''}</p>
-            <p>Email: {cvData ? cvData.email : ''}</p>
-            <p>Phone: {cvData ? cvData.phone : ''}</p>
+            <p>Name: {cvData ? cvData.name : ""}</p>
+            <p>Email: {cvData ? cvData.email : ""}</p>
+            <p>Phone: {cvData ? cvData.phone : ""}</p>
           </div>
         )}
-        <button onClick={isEditing ? handleSubmit : handleEdit}>{isEditing ? 'Submit' : 'Edit'}</button>
-      </div>
-      <div>
-        <h2>CV Preview</h2>
-        {cvData ? (
-          <div>
-            <p>Name: {cvData.name}</p>
-            <p>Email: {cvData.email}</p>
-            <p>Phone: {cvData.phone}</p>
-          </div>
-        ) : (
-          <p>Please submit to see your CV preview.</p>
-        )}
+        <button onClick={isEditing ? handleSubmit : handleEdit}>
+          {isEditing ? "Submit" : "Edit"}
+        </button>
       </div>
     </div>
   );
