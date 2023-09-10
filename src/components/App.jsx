a2d68f1cab794c6611cc216bb1a8681966535a14
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-10 22:23:34 +0530

    Added EducationInfo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GeneralInfo from './GeneralInfo'
+import Education from './EducationInfo'
 
 function App() {
   return (
   <div>
     <GeneralInfo/>
+    <Education />
   </div>
   )
 }
---
+++ src/components/EducationInfo.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+function Education() {
+  const [title, setTitle] = useState('');
+  const [schoolName, setSchoolName] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [cvData, setCvData] = useState(null);
+  const [isEditing, setIsEditing] = useState(true);
+
+  const handleSubmit = () => {
+    const cvInfo = {
+      title,
+      schoolName,
+      startDate,
+      endDate
+    };
+
+    setCvData(cvInfo);
+    setIsEditing(false); // Exit edit mode after submitting
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true); // Enter edit mode
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Education</h2>
+        {isEditing ? (
+          <div>
+            <input type="text" placeholder="Title of Study" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" placeholder="Name of Acedemic Institution" value={schoolName} onChange={(e) => setSchoolName(e.target.value)} />
+            <input type="text" placeholder="Start Date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+            <input type="text" placeholder="End Date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+            {/* <button onClick={handleSubmit}>Submit</button> */}
+          </div>
+        ) : (
+          <div>
+            <p>Title: {cvData ? cvData.title : ''}</p>
+            <p>School Name: {cvData ? cvData.schoolName : ''}</p>
+            <p>Start Date: {cvData ? cvData.startDate : ''}</p>
+            <p>End Date: {cvData ? cvData.endDate : ''}</p>
+          </div>
+        )}
+        <button onClick={isEditing ? handleSubmit : handleEdit}>{isEditing ? 'Submit' : 'Edit'}</button>
+      </div>
+      <div>
+        <h2>CV Preview</h2>
+        {cvData ? (
+          <div>
+            <p>Title: {cvData.title}</p>
+            <p>School Name: {cvData.schoolName}</p>
+            <p>Start Date: {cvData.startDate}</p>
+            <p>End Date: {cvData.endDate}</p>
+          </div>
+        ) : (
+          <p>Please submit to see your CV preview.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Education;
